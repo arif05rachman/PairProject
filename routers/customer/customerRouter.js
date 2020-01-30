8affed8d62d6416b24988860bed8d581d3058a7c
arif05rachman <arif05rachman@gmail.com>
2020-01-30 23:27:50 +0700

    menampilkan data list

--- routers/customer/customerRouter.js
+++ routers/customer/customerRouter.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const CustomerController = require('../../controllers/customer/customerController')
 
 router.get('/', CustomerController.index)
+router.post('/login', CustomerController.login)
 router.get('/home', CustomerController.home)
+router.get('/order/:id', CustomerController.order)
+router.get('/list', CustomerController.list)
 
 module.exports = router
